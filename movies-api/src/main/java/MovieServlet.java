d158321b1093f752f4cd983578503dcc7d931e58
Patrick Quilty <pquilty79@gmail.com>
2022-04-04 15:25:36 -0500

    set up movieservlet class

---
+++ movies-api/src/main/java/MovieServlet.java
@@ -0,0 +1,75 @@
+import com.google.gson.Gson;
+import data.Movie;
+
+import java.io.*;
+import java.util.ArrayList;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name = "MovieServlet", urlPatterns = "/movies/*")
+public class MovieServlet extends HttpServlet {
+
+    int nextId = 1;
+    ArrayList<Movie> movies = new ArrayList<>();
+    Movie princessBride = new Movie("The Princess Bride", "Rob Reiner", "Cary Elwes, Mandy Patinkin, Robin Wright", "Adventure, Family, Fantasy", 1987, 8.1, "An elderly man reads the book \"The Princess Bride\" to his sick and thus currently bedridden adolescent grandson, the reading of the book which has been passed down within the family for generations. The grandson is sure he won't like the story, with a romance at its core, he prefers something with lots of action and \"no kissing\", but he lets grandfather continue, because he doesn't want to hurt his feelings. The story centers on Buttercup, a former farm girl who has been chosen as the princess bride to Prince Humperdinck of Florian. Buttercup does not love him, she who still laments the death of her one true love, Westley, five years ago. Westley was a hired hand on the farm, his stock answer of \"as you wish\" to any request she made of him which she came to understand was his way of saying that he loved her. But Westley went away to sea, only to be killed by the Dread Pirate Roberts. On a horse ride to clear her mind of her upcoming predicament of marriage, Buttercup is kidnapped by a band of bandits: Vizzini who works on his wits, and his two associates, a giant named Fezzik who works on his brawn, and a Spaniard named Inigo Montoya, who has trained himself his entire life to be an expert swordsman. They in turn are chased by the Dread Pirate Roberts himself. But chasing them all is the Prince, and his men led by Count Tyrone Rugen. What happens to these collectives is dependent partly on Buttercup, who does not want to marry the Prince, and may see other options as lesser evils, and partly on the other motives of individuals within the groups. But a larger question is what the grandson will think of the story as it proceeds and at its end, especially as he sees justice as high a priority as action.", "https://imdb-api.com/images/original/MV5BMGM4M2Q5N2MtNThkZS00NTc1LTk1NTItNWEyZjJjNDRmNDk5XkEyXkFqcGdeQXVyMjA0MDQ0Mjc@._V1_Ratio0.6762_AL_.jpg", "https://www.youtube.com/watch?v=tD3WKbQRvQw", "1h 38min", "PG", true, 309);
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        String movieString = new Gson().toJson(movies.toArray());
+        try {
+            PrintWriter out = response.getWriter();
+            out.println(movieString);
+        } catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    protected void doPost (HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        Movie[] newMovies = new Gson().fromJson(request.getReader(), Movie[].class);
+        for (Movie movie : newMovies) {
+            movie.setId(nextId++);
+            movies.add(movie);
+        }
+        try {
+            PrintWriter out = response.getWriter();
+            out.println("Movie added");
+
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPut (HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("application/json");
+        try {
+            PrintWriter out = response.getWriter();
+
+
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doDelete (HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("application/json");
+        try {
+            PrintWriter out = response.getWriter();
+
+
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+
+
+
+
+}
---
+++ movies-api/src/main/java/data/Movie.java
@@ -0,0 +1,149 @@
+package data;
+
+public class Movie {
+
+    private String title;
+    private String director;
+    private String actors;
+    private String genre;
+    private int year;
+    private double rating;
+    private String plot;
+    private String poster;
+    private String trailerURL;
+    private String runtime;
+    private String MPAA;
+    private boolean favorite;
+    private int id;
+
+    public Movie() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public void setActors(String actors) {
+        this.actors = actors;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getTrailerURL() {
+        return trailerURL;
+    }
+
+    public void setTrailerURL(String trailerURL) {
+        this.trailerURL = trailerURL;
+    }
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
+
+    public String getMPAA() {
+        return MPAA;
+    }
+
+    public void setMPAA(String MPAA) {
+        this.MPAA = MPAA;
+    }
+
+    public boolean isFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+    public Movie(String title, String director, String actors, String genre, int year, double rating, String plot, String poster, String trailerURL, String runtime, String MPAA, boolean favorite, int id) {
+        this.title = title;
+        this.director = director;
+        this.actors = actors;
+        this.genre = genre;
+        this.year = year;
+        this.rating = rating;
+        this.plot = plot;
+        this.poster = poster;
+        this.trailerURL = trailerURL;
+        this.runtime = runtime;
+        this.MPAA = MPAA;
+        this.favorite = favorite;
+        this.id = id;
+    }
+
+
+
+
+
+
+}
