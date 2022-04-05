1c546aec6b9b26adcb2e00c4e29b877c138bfb27
Patrick Quilty <pquilty79@gmail.com>
2022-04-05 09:51:25 -0500

    added movie classes

---
+++ movies-api/src/main/java/data/MoviesDaoFactory.java
@@ -0,0 +1,27 @@
+package data;
+
+import data.InMemoryMoviesDao;
+import data.MoviesDao;
+//import data.MySqlMoviesDao;
+
+import java.io.ObjectInputFilter;
+
+public class MoviesDaoFactory {
+
+//    private static Config config = new Config();
+
+    public enum DAOType {MYSQL, IN_MEMORY}
+
+    ; //Notice we have two values here
+
+    public static MoviesDao getMoviesDao(DAOType daoType) {
+
+        switch (daoType) {
+            case IN_MEMORY: { //yet we have one switch case. We'll get to that!
+                return new InMemoryMoviesDao();
+            }
+        }
+        return null;
+    }
+}
+
