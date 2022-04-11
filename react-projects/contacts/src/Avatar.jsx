3f4d23105900770a48f35c3ff8a7935da1a10a49
Patrick Quilty <pquilty79@gmail.com>
2022-04-11 16:47:25 -0500

    Delete react-projects directory

--- react-projects/contacts/src/Avatar.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-function Avatar(props) {
-    return <img className="circle-img" src={props.img} alt="contact"/>
-}
-export default Avatar;
--- react-projects/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-let year = new Date().getFullYear()
-
-function Footer(){
-    return <footer><p>Copyright © {year}</p></footer>
-}
-
-export default Footer;
